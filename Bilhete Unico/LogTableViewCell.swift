2e93e938cf5499a1ff59f991a36d4120bff0713b
Matheus Pedrosa <math.pedrosa@hotmail.com>
2019-08-03 23:11:20 -0300

    initial commit

---
+++ Bilhete Unico/LogTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  LogTableViewCell.swift
+//  Bilhete Unico
+//
+//  Created by Matheus Pedrosa on 8/3/19.
+//  Copyright © 2019 M2P. All rights reserved.
+//
+
+import UIKit
+
+class LogTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        self.selectionStyle = .none
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setup(with tarifa: String, valor: String){
+        self.textLabel?.text = tarifa
+        self.detailTextLabel?.text = valor
+    }
+
+}
